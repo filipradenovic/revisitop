0141a3b0234f09a9fc79f0c2a5f7d6376cdc5281
Filip Radenovic <fradenovic@gmail.com>
2018-04-13 16:10:57 +0200

    python toolbox: configdataset functions added, example script to read images added

---
+++ python/dataset.py
@@ -0,0 +1,80 @@
+import os
+import numpy as np
+
+from scipy.io import loadmat
+
+def configdataset(dataset, dir_main):
+
+	dataset = dataset.lower()
+	if dataset == 'oxford5k' or dataset == 'paris6k':
+		cfg = {'ext' : '.jpg', 'qext' : '.jpg', 'dir_data' : os.path.join(dir_main, dataset)}
+		cfg['gnd_fname'] = os.path.join(cfg['dir_data'], 'gnd_' + dataset + '.mat')
+		gt = loadmat(cfg['gnd_fname'])
+		cfg['imlist'] = [str(''.join(im)) for iml in np.squeeze(gt['imlist']) for im in iml]
+		cfg['qidx'] = np.squeeze(gt['qidx']) - 1
+		cfg['qimlist'] = [cfg['imlist'][i] for i in cfg['qidx']]
+		cfg['gnd'] = gnd_mat2py(gt['gnd'])
+		cfg['n'] = len(cfg['imlist'])
+		cfg['nq'] = len(cfg['qimlist'])
+
+	elif dataset == 'roxford5k' or dataset == 'rparis6k':
+		cfg = {'ext' : '.jpg', 'qext' : '.jpg', 'dir_data' : os.path.join(dir_main, dataset)}
+		cfg['gnd_fname'] = os.path.join(cfg['dir_data'], 'gnd_' + dataset + '.mat')
+		gt = loadmat(cfg['gnd_fname'])
+		cfg['imlist'] = [str(''.join(im)) for iml in np.squeeze(gt['imlist']) for im in iml]
+		cfg['qimlist'] = [str(''.join(im)) for iml in np.squeeze(gt['qimlist']) for im in iml]
+		cfg['gnd'] = gnd_mat2py(gt['gnd'])
+		cfg['n'] = len(cfg['imlist'])
+		cfg['nq'] = len(cfg['qimlist'])		
+
+	else:
+		raise ValueError('Unknown dataset: %s!' % dataset)
+
+	cfg['dir_images'] = os.path.join(cfg['dir_data'], 'jpg')
+
+	cfg['im_fname'] = config_imname
+	cfg['qim_fname'] = config_qimname
+
+	cfg['dataset'] = dataset
+
+	return cfg
+
+def config_imname(cfg, i):
+	return os.path.join(cfg['dir_images'], cfg['imlist'][i] + cfg['ext'])
+
+def config_qimname(cfg, i):
+	return os.path.join(cfg['dir_images'], cfg['qimlist'][i] + cfg['qext'])
+
+def gnd_mat2py(gnd):
+	gnd = np.squeeze(gnd);
+	gndpy = []
+	for i in np.arange(len(gnd)):
+		gndi = gnd[i]
+		gndpyi = {}
+		try:
+			gndpyi['ok'] = gnd[i]['ok']-1
+			gndpyi['ok'] = gndpyi['ok'].reshape(gndpyi['ok'].shape[1])
+		except:
+			pass
+		try:
+			gndpyi['easy'] = gnd[i]['easy']-1
+			gndpyi['easy'] = gndpyi['easy'].reshape(gndpyi['easy'].shape[1])
+		except:
+			pass
+		try:
+			gndpyi['hard'] = gnd[i]['hard']-1
+			gndpyi['hard'] = gndpyi['hard'].reshape(gndpyi['hard'].shape[1])
+		except:
+			pass
+		try:
+			gndpyi['junk'] = gnd[i]['junk']-1
+			gndpyi['junk'] = gndpyi['junk'].reshape(gndpyi['junk'].shape[1])
+		except:
+			pass
+		try:
+			gndpyi['bbx'] = np.squeeze(gnd[i]['bbx'])
+		except:
+			pass
+		gndpy.append(gndpyi)
+
+	return gndpy
---
+++ python/example_read_images.py
@@ -0,0 +1,57 @@
+# EXAMPLE_READ_IMAGES  Code to read images for Oxford, ROxford, Paris, and RParis datasets.
+# Revisited protocol (ROxford and RParis) requires query images to be removed from the database, and cropped prior to any processing.
+# This code makes sure the protocol is strictly followed.
+#
+# More details about the revisited annotation and evaluation can be found in:
+# Radenovic F., Iscen A., Tolias G., Avrithis Y., Chum O., Revisiting Oxford and Paris: Large-Scale Image Retrieval Benchmarking, CVPR 2018
+#
+# Authors: Radenovic F., Iscen A., Tolias G., Avrithis Y., Chum O., 2018
+
+import os
+import numpy as np
+
+from PIL import Image
+
+from dataset import configdataset
+
+#---------------------------------------------------------------------
+# Set data folder and testing parameters
+#---------------------------------------------------------------------
+# Set data folder, change if you have downloaded the data somewhere else
+data_root = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'data')
+# Check, and, if necessary, download test data (Oxford and Pairs), 
+# original and revisited annotation, and example feature vectors for evaluation
+# download_test(data_root)
+
+# Set test dataset: oxford5k | roxford5k | paris6k | rparis6k
+test_dataset = 'roxford5k'
+
+#---------------------------------------------------------------------
+# Read images
+#---------------------------------------------------------------------
+
+def pil_loader(path):
+    # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
+    with open(path, 'rb') as f:
+        img = Image.open(f)
+        return img.convert('RGB')
+
+print('>> {}: Processing test dataset...'.format(test_dataset))	
+# config file for the dataset
+# separates query image list from database image list, if revisited protocol used
+cfg = configdataset(test_dataset, os.path.join(data_root, 'datasets'))
+
+# query images
+for i in np.arange(cfg['nq']):
+	qim = pil_loader(cfg['qim_fname'](cfg, i)).crop(cfg['gnd'][i]['bbx'])
+	##------------------------------------------------------
+	## Perform image processing here, eg, feature extraction
+	##------------------------------------------------------
+	print('>> {}: Processing query image {}'.format(test_dataset, i+1))
+
+for i in np.arange(cfg['n']):
+	im = pil_loader(cfg['im_fname'](cfg, i))
+	##------------------------------------------------------
+	## Perform image processing here, eg, feature extraction
+	##------------------------------------------------------
+	print('>> {}: Processing database image {}'.format(test_dataset, i+1))
